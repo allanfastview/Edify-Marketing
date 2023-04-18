32203d9ed72e234a0c49fd07d97fb54380e63f9a
Allan <dantas@mileva.io>
2023-04-18 15:14:45 -0300

    Upgrade: Plugin version

--- edify-marketing.php
+++ edify-marketing.php
@@ -4,7 +4,7 @@
  * Plugin Name: Edify Marketing
  * Description: Provê funcionlidades relacionadas a àrea de Marketing da Edify
  * Author: Edify Education
- * Version: 1.1
+ * Version: 1.2
  * URL: https://www.notion.so/edifyeducation/Edify-Marketing-51a42e692f494be89cf992fb46ee6f39?pvs=4
  */
 
